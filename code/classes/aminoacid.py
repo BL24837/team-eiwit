d319ced02bae79e5b3f231ed6edbced753fa3192
Daanpijfers <daanpijfers@gmail.com>
2025-01-10 11:52:16 +0100

    begin

---
+++ code/classes/aminoacid.py
@@ -0,0 +1,15 @@
+
+
+
+class Aminozuur:
+    """
+
+    
+
+
+    """
+    
+    def __init__(self, amino_type,index):
+        self.amino_type = amino_type
+        self.coordinates = [0,0,0]
+        self.index = index
--- code/classes/protein.py
+++ code/classes/protein.py
@@ -1,25 +1,8 @@
 
-
-class Aminozuur:
-    def __init__(self):
-        self.amino_type = # H, C, P;
-        self.index = # plaatst in sequentie;
-        self.x = # x coordinaat;
-        self.y = # y coordinaat;
-
-    def get_score_stabiliteit(self, other):
-        if self.amino_type == "H" and other.amino_type == "H":
-            return -1
-        elif self.amino_type == "C" and other.amino_type == "C":
-            return -5
-        elif (self.amino_type == "C" and other.amino_type == "H") or (self.amino_type == "H" and other.amino_type == "C"):
-            return -1
-        return 0
-
-class Eiwit:
-    def __init__(self):
-        self.sequentie = ;
-        self.aminozuren = []
-        self.grid_size = #lengte van de sequentie alle kanten op;
+class Protein:
+    def __init__(self, sequence:str, grid_size:int):
+        self.sequence = sequence
+        self.aminoacid = []
+        self.grid_size = grid_size
 
 
