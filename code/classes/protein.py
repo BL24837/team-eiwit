a49c29b0fe8b2889f27c3d0a0fc1a2a2144d9a9d
jlwW45-323tr2-12ttqFd <155000131+BL24837@users.noreply.github.com>
2025-01-10 13:30:07 +0100

    begin

--- code/classes/protein.py
+++ code/classes/protein.py
@@ -1,8 +1,9 @@
-
 class Protein:
     def __init__(self, sequence:str, grid_size:int):
         self.sequence = sequence
         self.aminoacid = []
         self.grid_size = grid_size
+
+    
 
 
---
+++ main.py
@@ -0,0 +1,10 @@
+from .code.classes.protein import Protein
+from .code.classes.aminoacid import Aminoacid
+from .code.visualisation.board import Board
+
+
+
+
+
+
+
