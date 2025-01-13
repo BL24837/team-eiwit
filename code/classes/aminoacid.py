901ddd7d43cdd89f88ed598a9c05236441e2b79d
jlwW45-323tr2-12ttqFd <155000131+BL24837@users.noreply.github.com>
2025-01-13 11:55:30 +0100

    beginnetje

--- code/classes/aminoacid.py
+++ code/classes/aminoacid.py
@@ -2,6 +2,19 @@
 class AminoAcid:
     def __init__(self, type_):
         self.type = type_  # 'H', 'P', or 'C'
+        self.front_neighbour = None
+        self.back_neighbour = None
+
+    def place_aminoacid():
+
+    def front_neighbour():
+
+    def back_neighbour():
+
+
+
+
+
 
     def __repr__(self):
         return f"AminoAcid(type='{self.type}')"
